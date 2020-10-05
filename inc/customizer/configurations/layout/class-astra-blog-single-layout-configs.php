f7796e76fa3b1dc4135d1176db9fa9d6f708f3d2
Hai Nguyen <haikbvn@gmail.com>
2020-10-05 20:17:15 +0700

    Update class-astra-blog-single-layout-configs.php
    Some options cannot set default values.

--- inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
+++ inc/customizer/configurations/layout/class-astra-blog-single-layout-configs.php
@@ -62,7 +62,7 @@ if ( ! class_exists( 'Astra_Blog_Single_Layout_Configs' ) ) {
 					'control'     => 'ast-slider',
 					'section'     => 'section-blog-single',
 					'transport'   => 'postMessage',
-					'default'     => 1200,
+					'default'     => astra_get_option( 'blog-single-max-width' ),
 					'required'    => array( ASTRA_THEME_SETTINGS . '[blog-single-width]', '===', 'custom' ),
 					'priority'    => 5,
 					'title'       => __( 'Custom Width', 'astra' ),
